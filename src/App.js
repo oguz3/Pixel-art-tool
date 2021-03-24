3ad3f9af7453c306e21608028961b3ac74615eb1
oğuzulukaya <55917459+oguz3@users.noreply.github.com>
2021-03-24 18:45:46 +0300

    Added icon & style

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import { useState } from 'react';
 import Canvas from './componenet/Canvas';
-import './style/App.css';
+import styles from './style/App.module.css';
+import Pencil from './icons/pencil';
+import Eraser from './icons/eraser';
 
 function App() {
   const [row, setRow] = useState(50)
-  const [coll, setColl] = useState(30)
+  const [coll, setColl] = useState(50)
   return (
-    <div className="App">
-        <input value={row} onChange={event => setRow(event.target.value)} />
-        <input value={coll} onChange={event => setColl(event.target.value)} />
-        <div className={}>
+    <div className={styles.App}>
+        <div className={styles.InputBox}>
+          <div className={styles.Input}>
+            <label>Width: </label>
+            <input value={row} onChange={event => setRow(event.target.value)} />
+          </div>
+          <div className={styles.Input}>
+            <label>Height: </label>
+            <input value={coll} onChange={event => setColl(event.target.value)} />
+          </div>
+          <Pencil />
+          <Eraser />
+        </div>
+
+        <div className={styles.CanvasBox}>
           <Canvas row={row} coll={coll}/>
+        </div>
+
+        <div className={styles.iconsBox}>
+          <div>Icons made by <a href="https://www.flaticon.com/authors/pongsakornred" title="pongsakornRed">pongsakornRed</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+          <div>Icons made by <a href="https://www.flaticon.com/authors/icongeek26" title="Icongeek26">Icongeek26</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
         </div>
     </div>
   );
