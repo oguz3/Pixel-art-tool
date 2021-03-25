382f0e94e4a7630ed82e40ea915713d9f62a0b81
oğuzulukaya <55917459+oguz3@users.noreply.github.com>
2021-03-25 21:39:31 +0300

    Update index.js

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ function App(props) {
         <div className={styles.iconsBox}>
           <div>Icons made by <a href="https://www.flaticon.com/authors/pongsakornred" title="pongsakornRed">pongsakornRed</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
           <div>Icons made by <a href="https://www.flaticon.com/authors/icongeek26" title="Icongeek26">Icongeek26</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-          <div>Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
         </div>
     </div>
   );
