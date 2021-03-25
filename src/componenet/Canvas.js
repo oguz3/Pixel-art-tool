2380b9d4201b78208f30851ff2db20b5184f6431
oğuzulukaya <55917459+oguz3@users.noreply.github.com>
2021-03-25 19:54:22 +0300

    Added mouseMove draw

--- src/componenet/Canvas.js
+++ src/componenet/Canvas.js
@@ -1,32 +1,42 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import { connect } from 'react-redux';
 import drawMatrix from '../functions/drawMatrix';
 import draw from '../functions/draw';
 
 function Canvas(props){
   const canvasRef = useRef(null)
+  const contextRef = useRef(null)
+  const[isDown, setIsDown] = useState(false);
   
   useEffect(() => {
     const canvas = canvasRef.current
     const context = canvas.getContext('2d')
-
+    contextRef.current = context;
     drawMatrix(context, props.row, props.coll, props.cellSize);
-
-    const clickCanvas = (e) => {
-      draw(canvas, context, e, props.row, props.coll, props.cellSize)
-    };
-
-    document.addEventListener("click", (e) => clickCanvas(e));
-    return () => {
-      document.removeEventListener("click", (e) => clickCanvas(e));
-    };
   }, [props.row, props.coll, props.cellSize])
+
+  const mouseDown = (e) => {
+    setIsDown(true);
+    drawCanvas(e)
+  }
+  const mouseUp = (e) => {
+    drawCanvas(e)
+    setIsDown(false);
+  }
+  const drawCanvas = (e) => {
+    if(isDown === true){
+      draw(canvasRef.current, contextRef.current, e, props.row, props.coll, props.cellSize)
+    }
+  }
   
   return (
     <canvas 
         ref={canvasRef}
         width={15*props.row}
         height={15*props.coll}
+        onMouseMove={(e) => drawCanvas(e)}
+        onMouseDown={(e) => mouseDown(e)}
+        onMouseUp={(e) => mouseUp(e)}
     />
   );
 }
